2bba81d50266c52cd7cba3f07aef652db7d467c9
gurkanozil <gurkanozil@live.nl>
2025-02-15 20:12:37 +0100

    Refactor 'Over ons' page layout and update menu structure

--- src/over_ons.php
+++ src/over_ons.php
@@ -35,22 +35,11 @@ include 'header.php';
 ?>
 <h1>Over ons</h1>
 
-<div class="row intro-segment __web-inspector-hide-shortcut__">
-<div class="col-xs-10 col-xs-offset-1">
-    <div class="h1 no-margin text-center">Suitable staat voor</div>
-    <div class="text-center">
-    </div>
-</div>
-</div>
-<div class="row margin--2x margin-bottom-2x">
+<div class="h1 no-margin text-center">Suitable staat voor</div>
+
 <div class="col-xs-12 col-sm-4 padding-half">
-    <div class="block-content content-icon">
-        <div class="content">
-            <div class="icon suitableicon-people"></div>
-            <div class="title">De man in zijn kracht</div>
-            <p class="content-height flex">We begrijpen wat hij wil en hoe we hem het beste kunnen helpen</p>
-        </div>
-    </div>
+    <div class="title">De man in zijn kracht</div>
+    <p class="content-height flex">We begrijpen wat hij wil en hoe we hem het beste kunnen helpen</p>
 </div>
 
 <div class="col-xs-12 col-sm-4 padding-half">
@@ -62,70 +51,65 @@ include 'header.php';
         </div>
     </div>
 </div>
-
 <div class="col-xs-12 col-sm-4 padding-half">
     <div class="block-content content-icon">
-        <div class="content">
-            <div class="icon suitableicon-warrant"></div>
-            <div class="title">Kwaliteit</div>
-            <p class="content-height flex">Onze collectie is van hoge kwaliteit met een degelijke prijs.</p>
-        </div>
-    </div>
-</div>
-</div>
-<div class="row intro-segment">
-
-
-    <div class="h1 no-margin text-center">Kernwaarden</div>
-    <div class="text-center">
-        <div class="spacer-half"></div>
-    </div>
-
-
-    <div class="row margin--2x">
-        <div class="col-xs-12 col-sm-4 padding-half">
-            <div class="block-content content-icon">
                 <div class="content">
-                    <div class="title">Betrokken</div>
-                    <p class="content-height flex">Binding met de klant, breed geïnteresseerd en collegialiteit.</p>
+                    <div class="icon suitableicon-warrant"></div>
+                    <div class="title">Kwaliteit</div>
+                    <p class="content-height flex">Onze collectie is van hoge kwaliteit met een degelijke prijs.</p>
+                </div>
+            </div>
+        </div>    
+        
+        <div class="h1 no-margin text-center">Kernwaarden</div>
+        <div class="text-center">
+            <div class="spacer-half"></div>
+        </div>
+                
+        <div class="row margin--2x">
+            <div class="col-xs-12 col-sm-4 padding-half">
+                <div class="block-content content-icon">
+                    <div class="content">
+                        <div class="title">Betrokken</div>
+                        <p class="content-height flex">Binding met de klant, breed geïnteresseerd en collegialiteit.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-xs-12 col-sm-4 padding-half">
+                <div class="block-content content-icon">
+                    <div class="content">
+                        <div class="title">Innovatief</div>
+                        <p class="content-height flex">Vernieuwende producten, materialen en denken in oplossingen.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-xs-12 col-sm-4 padding-half">
+                <div class="block-content content-icon">
+                    <div class="content">
+                        <div class="title">Krachtig</div>
+                        <p class="content-height flex">Goede kwaliteit leveren en daadkrachtig zijn.</p>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-xs-12 col-sm-4 padding-half">
-            <div class="block-content content-icon">
-                <div class="content">
-                    <div class="title">Innovatief</div>
-                    <p class="content-height flex">Vernieuwende producten, materialen en denken in oplossingen.</p>
-                </div>
-            </div>
+            
+    <h1>Contactformulier</h1>
+    <form action="over_ons.php" method="post">
+        <div class="mb-3">
+            <label for="name" class="form-label">Naam</label>
+            <input type="text" class="form-control" id="name" name="name" required>
         </div>
-
-        <div class="col-xs-12 col-sm-4 padding-half">
-            <div class="block-content content-icon">
-                <div class="content">
-                    <div class="title">Krachtig</div>
-                    <p class="content-height flex">Goede kwaliteit leveren en daadkrachtig zijn.</p>
-                </div>
-            </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">E-mail</label>
+            <input type="email" class="form-control" id="email" name="email" required>
         </div>
-    </div>
-
-<h1>Contactformulier</h1>
-<form action="over_ons.php" method="post">
-    <div class="mb-3">
-        <label for="name" class="form-label">Naam</label>
-        <input type="text" class="form-control" id="name" name="name" required>
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">E-mail</label>
-        <input type="email" class="form-control" id="email" name="email" required>
-    </div>
-    <div class="mb-3">
-        <label for="message" class="form-label">Bericht</label>
-        <textarea class="form-control" id="message" name="message" rows="3" required></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Verstuur</button>
-</form>
-
-<?php include 'footer.php'; ?>
+        <div class="mb-3">
+            <label for="message" class="form-label">Bericht</label>
+            <textarea class="form-control" id="message" name="message" rows="3" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Verstuur</button>
+    </form>
+    
+    <?php include 'footer.php'; ?>
